80be6661b14de853be78276c60c280cfe020ece2
Christoph Herr <info@christophherr.com>
2018-08-05 14:21:25 -0400

    Delete with Context API

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../context';
 
 class Contact extends Component {
   state = {
@@ -17,55 +18,69 @@ class Contact extends Component {
     this.setState({ showContactInfo: !this.state.showContactInfo });
   };
 
-  onClickDelete = () => {
-    this.props.deleteClickHandler();
+  onKeyDelete = (e, id, dispatch) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
+    }
+  };
+
+  onClickDelete = (id, dispatch) => {
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
   };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
     return (
-      <div className="card card-body mb-3">
-        <div className="card-title">
-          <h4>{name}</h4>
-          <button
-            onClick={this.onClickToggle}
-            onKeyPress={this.onKeyToggle}
-            className="btn btn-primary ml-3"
-            aria-expanded={showContactInfo}
-            aria-pressed={showContactInfo}
-          >
-            <span
-              className={
-                showContactInfo
-                  ? 'fas fa-sort-up mr-2'
-                  : 'fas fa-sort-down mr-2'
-              }
-            />
-            {showContactInfo ? 'Hide Details' : 'Show Details'}
-          </button>
-          <button
-            className="btn btn-danger ml-auto"
-            onClick={this.onClickDelete}
-          >
-            <span className="fas fa-times mr-2" />
-            Delete Contact
-          </button>
-        </div>
-        {showContactInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
-        ) : null}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <div className="card-title">
+                <h4>{name}</h4>
+                <button
+                  onClick={this.onClickToggle}
+                  onKeyPress={this.onKeyToggle}
+                  className="btn btn-primary ml-3"
+                  aria-expanded={showContactInfo}
+                  aria-pressed={showContactInfo}
+                >
+                  <span
+                    className={
+                      showContactInfo
+                        ? 'fas fa-sort-up mr-2'
+                        : 'fas fa-sort-down mr-2'
+                    }
+                  />
+                  {showContactInfo ? 'Hide Details' : 'Show Details'}
+                </button>
+                <button
+                  className="btn btn-danger ml-auto"
+                  onClick={this.onClickDelete.bind(this, id, dispatch)}
+                  onKeyPress={this.onKeyDelete.bind(this, id, dispatch)}
+                >
+                  <span className="fas fa-times mr-2" />
+                  Delete Contact
+                </button>
+              </div>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired,
-  deleteClickHandler: PropTypes.func.isRequired
+  contact: PropTypes.object.isRequired
 };
 
 export default Contact;
