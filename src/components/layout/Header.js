e4e2630e73bfa3d96d977f9e6e0f68187a337a64
Christoph Herr <info@christophherr.com>
2018-08-05 21:38:57 -0400

    Added Routes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Header = props => {
   return (
     <header>
       <nav className="navbar navbar-expand-sm navbar-dark bg-info mb-3 py-0">
         <div className="container">
-          <a href="/" className="navbar-brand">
+          <Link to="/" className="navbar-brand">
             {props.branding}
-          </a>
+          </Link>
           <div>
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a href="/" className="nav-link">
+                <Link to="/" className="nav-link">
+                  <span className="fas fa-home mr-1" />
                   Home
-                </a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact/add" className="nav-link">
+                  <span className="fas fa-plus mr-1" />
+                  Add Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-link">
+                  <span className="fas fa-question mr-1" />
+                  About
+                </Link>
               </li>
             </ul>
           </div>
